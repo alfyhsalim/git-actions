8dace0a424ac90740665ca06d803d28533ba8737
Alfy <alfy.salim@mphasis.com>
2026-08-07 13:00:52 +0000

    health checks pom.xml

--- src/main/java/test/CalculatorApplication.java
+++ src/main/java/test/CalculatorApplication.java
@@ -2,7 +2,11 @@ package test;
 
 public class CalculatorApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Calculator POC Running");
+
+        while (true) {
+            Thread.sleep(60000);
+        }
     }
 }
