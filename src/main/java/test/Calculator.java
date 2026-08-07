5cfb93ff216d3e6e4091116f4d39f332f6e808f7
Alfy <alfy.salim@mphasis.com>
2026-08-07 12:52:32 +0000

    code coverage corrected and health checks

--- src/main/java/test/Calculator.java
+++ src/main/java/test/Calculator.java
@@ -15,3 +15,10 @@ public class Calculator {
         return "negative";
     }
 }
+
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Calculator POC Running");
+    }
+}
