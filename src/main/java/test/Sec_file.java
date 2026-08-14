bc18d5281ee32d3b574c6f4f8b6ac174c9646011
Alfy <alfy.salim@mphasis.com>
2026-08-14 09:09:44 +0000

    Testing secret scan

---
+++ src/main/java/test/Sec_file.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Sec_file {
+    private static final String VAR = "ghp_123456789012345678901234567890123456";
+
+	public static final String AWS_ACCESS_KEY_ID = "AKIA1234567890ABCDEF";
+	public static final String AWS_SECRET_ACCESS_KEY = "abcdefghijklmnopqrstuvwxyz1234567890ABCD";
+}
